db0fc596e3ae3c4e4b1535c6cf09c42776eb0796
RivkaRamaty <rivkiry@gmail.com>
2019-09-06 05:53:01 +0200

    Changes: class_info screen

--- src/screens/class_info_screen.js
+++ src/screens/class_info_screen.js
@@ -1,26 +1,27 @@
-import React, { Component, userState } from 'react';
+import React, { Component } from 'react';
 import {
-    SafeAreaView,
     StyleSheet,
     ScrollView,
     View,
     Text,
-    TextInput,
-    StatusBar,
-    Button,
     TouchableOpacity,
     Image,
-    FlatList,
-    Dimensions,
+    ImageBackground,
+    TouchableHighlight
 } from 'react-native';
 import { Linking } from 'expo';
+import Modal from 'react-native-modal';
+import DisplayGrade from '../components/DisplayGrade';
+import ClassInfo from '../components/ClassInfo';
+import Messages from '../components/Messages';
+import ResourceFiles from '../components/ResourceFiles';
+import Participants from '../components/Participants';
 
 
 export default class class_info_screen extends Component {
 
     constructor(props) {
         super(props);
-
 
         this.state = {
             messages: [
@@ -57,132 +58,159 @@ export default class class_info_screen extends Component {
                 { id: '8', type: 'file', source: '', name: 'File' },
                 { id: '9', type: 'link', source: '', name: 'Link' },
             ],
+            grades: [
+                { subject: 'Math', grade: '91' },
+                { subject: 'Science', grade: '90' },
+                { subject: 'Literature', grade: '82' },
+                { subject: 'Sport', grade: '100' },
+                { subject: 'Math1', grade: '91' },
+                { subject: 'Science1', grade: '90' },
+                { subject: 'Literature1', grade: '82' },
+                { subject: 'Sport1', grade: '100' },
+                { subject: 'Math2', grade: '91' },
+                { subject: 'Science2', grade: '90' },
+                { subject: 'Literature2', grade: '82' },
+                { subject: 'Sport2', grade: '100' },
+            ],
             quizes: 'https://play.kahoot.it/v2/lobby?quizId=ff490155-b7f8-40cc-92b3-4243b6b9487f',
+            modalVisible: false,
 
         }
     }
 
+    setModalVisible(visible) {
+        this.setState({ modalVisible: visible });
+    }
+
+    renderGrades() {
+        return (
+            this.state.grades.map(grades =>
+                <DisplayGrade
+                    key={grades.subject}
+                    grades={grades}
+                />
+            )
+        )
+    }
+
+    renderClassInfo() {
+        return (
+            <ClassInfo
+                icon={this.state.icon}
+                classIcon={require('../../assets/classIcon.png')}
+                name={this.state.name}
+                time={this.state.time}
+                location={this.state.location}
+                teacher={this.state.teacher}
+            />
+        )
+    }
+
+    renderMessages() {
+        return (
+            <Messages
+                messages={this.state.messages}
+            />
+        )
+    }
+
+    renderResources() {
+        return (
+            <ResourceFiles
+                data={this.state.items}
+            />
+        )
+    }
+
+    renderParticipants() {
+        return (
+            <Participants
+                data={this.state.participants}
+            />
+        )
+    }
+
     render() {
         return (
-            <View>
-                <ScrollView
-                    showsVerticalScrollIndicator={false}
-                >
-                    <View style={styles.container} >
-                        <View>
-                            <Image
-                                style={styles.classIcon}
-                                source={require('../../assets/classIcon.png')}
-                            /></View>
-                        <View style={{ alignItems: 'flex-start' }}>
-                            <Image
-                                style={styles.userIcon}
-                                source={this.state.icon}
-                            />
-                            <Text style={{ fontSize: 25 }}>{this.state.name}</Text>
-                            <Text>Time: {this.state.time}</Text>
-                            <Text>Location:</Text>
-                            <Text>{this.state.location}</Text>
-                            <Text>Teacher:</Text>
-                            <Text>{this.state.teacher}</Text>
-                        </View>
-                    </View>
-
-                    <View style={styles.messageInput}>
-                        <ScrollView
-                        nestedScrollEnabled
+            <ImageBackground
+                source={require('../../assets/background.jpeg')}
+                style={{ width: '100%', height: '100%' }}>
+                <View>
+                    <ScrollView
                         showsVerticalScrollIndicator={false}
-                        >
-                        <FlatList
-                            keyExtractor={(message) => message.content}
-                            showsVerticalScrollIndicator
-                            data={this.state.messages}
-                            showsVerticalScrollIndicator={false}
-                            renderItem={({ item }) => {
-                                return (
-                                    <View><Text>{item.content + '\n'}</Text></View>
-                                );
-                            }}
-                        /></ScrollView>
-                    </View>
+                    >
+                        <View>{this.renderClassInfo()}</View>
+                        <View>{this.renderMessages()}</View>
+                        <View>{this.renderResources()}</View>
 
-                    <View style={styles.containerFiles}>
-                        <Text style={styles.textStyle}>Resources</Text>
-                        <FlatList
-                            numColumns={4}
-                            keyExtractor={(file) => file.id}
-                            data={this.state.items}
-                            renderItem={({ item }) => {
-                                icon = (item.type) === 'file' ? require('../../assets/fileIcon.png') : require('../../assets/linkIcon.png')
-                                return (
-                                    <View style={{ height: 75, width: 75 }}>
-                                        <TouchableOpacity>
-                                            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                                                <Image
-                                                    style={styles.userIcon}
-                                                    source={icon}
-                                                />
-                                                <Text>{item.name}</Text></View>
-                                        </TouchableOpacity>
-                                    </View>
-                                );
-                            }}
-                        />
-                    </View>
-
-                    <View style={{ flexDirection: 'row', }}>
-                        <View style={{ flex: 1, alignSelf: 'flex-start' }}>
-                            <TouchableOpacity
-                                onPress={() => {
-                                    Linking.openURL(this.state.quizes).catch((err) => console.error('An error occurred', err));
-                                }}>
-                                <View>
-                                    <Image
-                                        style={styles.classIcon}
-                                        source={require('../../assets/credit-kahoot.png')}
-                                    />
-                                </View>
-                            </TouchableOpacity></View>
-                        <View style={{ flex: 1, alignSelf: 'flex-end' }}>
-                            <TouchableOpacity>
-                                <View>
-                                    <Image
-                                        style={styles.classIcon}
-                                        source={require('../../assets/grades-icon.png')}
-                                    />
-                                </View>
-                            </TouchableOpacity></View>
-                    </View>
-
-                    <View>
-                        <Text style={styles.textStyle}>class info</Text>
-                    </View>
-
-
-                    <TouchableOpacity>
-                        <Text style={styles.textStyle}>Participants</Text>
-                    </TouchableOpacity>
-                    <View>
-                        <FlatList
-                            keyExtractor={(student) => student.name}
-                            horizontal
-                            showsHorizontalScrollIndicator={false}
-                            data={this.state.participants}
-                            renderItem={({ item }) => {
-                                return (
+                        <View style={{ flexDirection: 'row', }}>
+                            <View style={{ flex: 1, alignSelf: 'flex-start' }}>
+                                <TouchableOpacity
+                                    onPress={() => {
+                                        Linking.openURL(this.state.quizes).catch((err) => console.error('An error occurred', err));
+                                    }}>
                                     <View>
-                                        <View style={styles.container}>
-                                            <Image
-                                                style={styles.userIcon}
-                                                source={require('../../assets/user.png')}
-                                            /></View>
-                                        <Text style={styles.textStyle}>{item.name}</Text></View>
-                                );
-                            }}
-                        />
-                    </View>
-                </ScrollView>
-            </View >
+                                        <Image
+                                            style={styles.classIcon}
+                                            source={require('../../assets/credit-kahoot.png')}
+                                        />
+                                    </View>
+                                </TouchableOpacity></View>
+
+                            <View style={{ flex: 1, alignSelf: 'flex-end' }}>
+                                <TouchableOpacity
+                                    onPress={() => {
+                                        this.setModalVisible(!this.state.modalVisible);
+                                    }}
+                                >
+                                    <View>
+                                        <Image
+                                            style={styles.classIcon}
+                                            source={require('../../assets/grades-icon.png')}
+                                        />
+                                    </View>
+                                </TouchableOpacity></View>
+                        </View>
+
+                        <View style={{ flex: 1 }}>
+
+                            <Modal
+                                scroll inside the modal
+                                isVisible={this.state.modalVisible}
+                            >
+                                <View style={{
+                                    backgroundColor: '#f0f8ff', borderRadius: 15,
+                                    height: 500
+                                }}>
+
+                                    <View style={styles.headerStyle}>
+                                        <Text style={styles.headerTextStyle}>Grades</Text>
+                                    </View>
+                                    <ScrollView
+                                        showsVerticalScrollIndicator={false}
+                                        nestedScrollEnabled
+                                    >
+                                        <View>
+                                            {this.renderGrades()}
+                                        </View>
+                                    </ScrollView>
+                                    <View style={{ justifyContent: 'flex-end', alignItems: 'flex-end', marginTop: 15, marginRight: 10, marginBottom: 10 }}>
+                                        <TouchableHighlight
+                                            style={{ justifyContent: 'flex-end', alignItems: 'flex-end' }}
+                                            onPress={() => {
+                                                this.setModalVisible(!this.state.modalVisible);
+                                            }}>
+                                            <Text style={{ fontSize: 18 }}>Close</Text>
+                                        </TouchableHighlight></View>
+
+                                </View>
+                            </Modal>
+                        </View>
+
+                        <View>{this.renderParticipants()}</View>
+                    </ScrollView>
+                </View >
+            </ImageBackground >
         );
     }
 }
@@ -197,14 +225,13 @@ const styles = StyleSheet.create({
     },
     containerFiles: {
         justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#c3c4be'
+        alignItems: 'center'
     },
     containerMessages: {
         height: 180,
         borderWidth: 4,
         borderColor: 'black',
-        backgroundColor: '#92f7e5'
+        borderRadius: 5
     },
     messageInput: {
         flex: 1,
@@ -224,5 +251,18 @@ const styles = StyleSheet.create({
     classIcon: {
         width: 200,
         height: 200
-    }
+    },
+    headerTextStyle: {
+        marginHorizontal: 15,
+        fontSize: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#fff8dc',
+        fontWeight: 'bold'
+    },
+    headerStyle: {
+        backgroundColor: '#696969',
+        borderRadius: 15,
+        marginBottom: 5
+    },
 });
