d386604ef370b1bc928fbfe987b84a29f7b96170
Natanel Mor <natanelmo@mta.ac.il>
2019-09-07 14:04:06 +0300

    transform my profile component to class

--- src/screens/my_profile_screen.js
+++ src/screens/my_profile_screen.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { Text, StyleSheet, View, ImageBackground, Image} from 'react-native';
+import React, { Component} from 'react';
+import { Text, StyleSheet, View, TouchableOpacity, ImageBackground, Image} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import ClassesList from '../components/ClassesList';
 import AddClassButton from '../components/AddClassButton';
 
-const my_profile_screen = ({navigation}) => {
-  const passUser = navigation.getParam('user');
+class my_profile_screen extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {}
+  
+  passUser = this.props.navigation.getParam('user');
+  }
+  render(){
 
-  return (
-    <ImageBackground
-      source= {require('../../assets/background.jpeg')} 
-      style={{width:'100%', height:'100%' }}>
-    <View style={styles.container}>
-    <Image source={require('../../assets/logo.png')}/>
-    <Text style={styles.text}>Hello {passUser.name}!</Text>
-      <ScrollView showsVerticalScrollIndicator={false}>
-            <ClassesList user= {passUser}/>
-            <AddClassButton user= {passUser}/>
-      </ScrollView>
-    </View>
-    </ImageBackground>
-  )
-};
+    return (
+      <ImageBackground
+        source= {require('../../assets/background.jpeg')} 
+        style={{width:'100%', height:'100%' }}>
+      <View style={styles.container}>
+      <Image source={require('../../assets/logo.png')}/>
+      <Text style={styles.text}>Hello {passUser.name}!</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.classes}>
+              <ClassesList user= {passUser}/>
+              <AddClassButton user= {passUser}/>
+          </View>
+          <TouchableOpacity style={styles.touchOp} onPress ={() => this.props.navigation.navigate('my_classes',{ user : passUser} ) }  >
+            <Text>classes </Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+      </ImageBackground>
+    )
+  };
+}
 
 const styles = StyleSheet.create({
   text: {
