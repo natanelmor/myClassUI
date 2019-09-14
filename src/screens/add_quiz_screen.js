57e512437ad02e62fea4f21c11ae79dc52f9eb82
Kobi Shpak <kobishpak@gmail.com>
2019-09-14 22:27:09 +0300

    When a teacher creates a quiz return to a reasonable place

--- src/screens/add_quiz_screen.js
+++ src/screens/add_quiz_screen.js
@@ -59,7 +59,7 @@ class add_quiz_screen extends Component {
                // console.log(this.state.class);
                 axios.patch('https://myclass-backend.herokuapp.com/class?id=' + this.state.id, this.state.class)//update the class
                     .then(response => {}).catch(e => { console.log(e); });
-                this.props.navigation.navigate('my_profile');
+                this.props.navigation.navigate('quiz_time');
             
             
             
