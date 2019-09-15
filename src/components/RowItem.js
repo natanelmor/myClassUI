bf28cc071ba02a160536e66ec3479161f69d7678
Kobi Shpak <kobishpak@gmail.com>
2019-09-16 01:36:35 +0300

    WIP delete quiz

--- src/components/RowItem.js
+++ src/components/RowItem.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import globalStyle from '../style';
+import axios from 'axios';
 
-export const RowItem = ({ onPress = () => {}, name, color }) => {
+export const RowItem = ({ onPress = () => {}, name, color, userType, classID, quizID}) => {
   return(
    <TouchableOpacity style={[globalStyle.listItem, {backgroundColor: color}]} 
-   onPress={onPress} activeOpacity={0.8}>
+   onPress={onPress} activeOpacity={0.8}
+   onLongPress={({userType}) => {
+      if (userType === 'Teacher'){
+        axios.get('https://myclass-backend.herokuapp.com/class?id=' + classID)
+        .then(res => {
+          var index = res.data.quizes.indexOf(quizID)
+          if (index !== -1) {
+            res.data.quizes.splice(index, 1);
+            axios.patch('https://myclass-backend.herokuapp.com/class?id=' + classID, res.data)
+            .catch(err => {console.log(err)})
+          }
+        })
+      }
+   }}>
     <View style={styles.row}>
       <Text style={globalStyle.textCenter}>{name}</Text>
     </View>
